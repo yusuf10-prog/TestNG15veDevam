a84c4773ae09187055dece5c57416fdf3e446404
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-06-24 00:28:30 +0300

    24.06.2024

---
+++ src/test/java/tests/day15_testNG_framework/C01_Priority.java
@@ -0,0 +1,45 @@
+package tests.day15_testNG_framework;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class C01_Priority {
+
+    int a=20;
+    int b=10;
+
+    @Test(priority = -5)
+    public void carpmaTesti(){
+        // sayilarin carpiminin 100'den buyuk oldugunu test edin
+
+        Assert.assertTrue(a*b>100);
+    }
+    @Test
+    public void toplamaTesti(){
+        // sayilarin toplaminin pozitif bir sayi oldgunu test edin
+
+        Assert.assertTrue((a+b)>0);
+    }
+    @Test(priority = 53)
+    public void cikarmaTesti(){
+        // a ile b'nin farkinin mutlaka degerinin 100'den kucuk oldugunu test edin.
+        Assert.assertTrue((a-b)>-100 & (a-b)<100);
+    }
+
+    /*
+
+    TestNG hiçbir mudahale olmazsa
+    testleri isim sirasina gore calistirir
+
+    testlerin calisma siralamasini istedigimiz sekilde duzenleyebiliriz.
+
+    Testlere veregimiz piority degerlerinden
+    kucukten buyuge dogru calistirir
+
+    priority atamazsak TestNG default olarak priority=0 alir
+    ayni priority degerine sahip, birden fazla test method'u varsa
+    bunlar kendi icerisinde isim sirasina göre calisir.
+
+
+     */
+}
---
+++ src/test/java/utilities/Driver.java
@@ -0,0 +1,31 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class Driver {
+    /* JUnit' de WebDriver objesi TestBase'den geliyordu.
+
+   TestNG extends ile baglanma zorunlulugunu ortadan kaldirmak
+   ve testi yazanlara daha fazla kontrol imkani vermek icin
+   TestBase yerine Driver class'indan static 2 method ile
+   driver olusturma ve kapatma islemlerini yapmayi tercih etmistir.
+     */
+    static WebDriver driver;
+    public static WebDriver getDriver(){
+
+      if (driver==null){
+          driver=new ChromeDriver();
+      }
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(15));
+        return driver;
+    }
+
+    public static void closeDriver(){
+        driver.close();
+    }
+}
