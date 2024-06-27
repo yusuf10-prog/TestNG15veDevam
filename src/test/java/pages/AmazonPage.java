59790c6283ff72aa90edf7f43b3d1e5dd3a34315
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-06-28 02:24:20 +0300

    düzenleme

---
+++ src/test/java/pages/AmazonPage.java
@@ -0,0 +1,11 @@
+package pages;
+
+public class AmazonPage {
+    /*
+    TestNG'de page class'lari
+    surekli kullandigimiz bir sayfa / webSitesi
+    locate'leri ve
+    ilgili sayfada kullanilicak login gibi basit method'lari icerir.
+
+     */
+}
---
+++ src/test/java/tests/day16_testNG_Framework/C02_PageClassKullanimi.java
@@ -0,0 +1,22 @@
+package tests.day16_testNG_Framework;
+
+import org.testng.annotations.Test;
+import utilities.Driver;
+
+public class C02_PageClassKullanimi {
+    @Test
+    public void nutellaTesti(){
+        // amazona gidelim
+        Driver.getDriver().get("https://www.amazon.com.tr");
+
+
+        // Nutella icin arama yapalim
+
+
+        // arama sonuclarinin Nutella icerdigini test edelim
+
+
+        // driver'i kapatalim
+
+    }
+}
