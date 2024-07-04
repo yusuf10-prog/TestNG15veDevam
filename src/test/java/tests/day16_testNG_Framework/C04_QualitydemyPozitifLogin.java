f55717046cbea5d6992d343b02b1e1155f139291
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-07-05 00:52:04 +0300

    ensonyeni

--- src/test/java/tests/day16_testNG_Framework/C04_QualitydemyPozitifLogin.java
+++ src/test/java/tests/day16_testNG_Framework/C04_QualitydemyPozitifLogin.java
@@ -1,15 +1,31 @@
 package tests.day16_testNG_Framework;
 
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.Keys;
 import org.testng.annotations.Test;
+import pages.QualitydemyPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
 
 public class C04_QualitydemyPozitifLogin {
     @Test
     public void pozitifLoginTesti(){
         // 1- https://www.qualitydemy.com/ anasayfasina gidin
+        Driver.getDriver().get("https://www.qualitydemy.com/ ");
+
+
         // 2- login linkine basin
+        QualitydemyPage qualitydemyPage=new QualitydemyPage();
+
+        qualitydemyPage.ilkLoginLinki.click();
+
         // 3- Kullanici email'i olarak valid email girin
+        qualitydemyPage.emailKutusu.sendKeys("anevzatcelik@gmail.com");
+
         // 4- Kullanici sifresi olarak valid sifre girin
+        qualitydemyPage.passwordKutusu.sendKeys("Nevzat152032");
         // 5- Login butonuna basarak login olun
+        qualitydemyPage.loginButonu.click();
         // 6- Basarili olarak giris yapilabildigini test edin
 
     }
