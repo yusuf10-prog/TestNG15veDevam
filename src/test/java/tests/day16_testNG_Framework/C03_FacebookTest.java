bf544fae51c09a22d5dfebbe4b922e71b1bbd2a2
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-07-04 15:43:48 +0300

    yeni

---
+++ src/test/java/tests/day16_testNG_Framework/C03_FacebookTest.java
@@ -0,0 +1,33 @@
+package tests.day16_testNG_Framework;
+
+import com.github.javafaker.Faker;
+import org.testng.annotations.Test;
+import pages.FacebookPage;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_FacebookTest {
+    @Test
+    public void facebookTesti(){
+        // 1- https://www.facebook.com/ adresine gidin
+
+        Driver.getDriver().get("https://www.facebook.com/");
+
+
+        //2- POM'a uygun olarak email, sifre kutularini ve giris yap butonunu locate edin
+
+        ReusableMethods.bekle(1);
+
+
+        //3-Faker class'ini kullanarak email ve sifre degerlerini yazdirip, giris butonuna basin
+
+        Faker faker=new Faker();
+        FacebookPage facebookPage=new FacebookPage();
+
+        facebookPage.emailKutusu.sendKeys(faker.internet().emailAddress());
+        facebookPage.passwordKutusu.sendKeys(faker.internet().password());
+        facebookPage.loginButton.click();
+
+        //4- Basarili giris yapilamadigini test edin.
+    }
+}
