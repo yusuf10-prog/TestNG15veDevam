2d495b45c9be73a110510edde133cee2248a6265
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-07-23 23:26:32 +0300

    23.07.2024

---
+++ src/test/java/tests/day17_testNGFramework_assertions/C03_DriverSingletonPattern.java
@@ -0,0 +1,32 @@
+package tests.day17_testNGFramework_assertions;
+
+import org.testng.annotations.Test;
+import utilities.ConfigReader;
+import utilities.Driver;
+import utilities.ReusableMethods;
+
+public class C03_DriverSingletonPattern {
+    @Test
+    public void test01(){
+        /*
+        POM kapsaminda
+        locate'lar, test datalari ve WebDriver'i dinamikleştirdik
+        hepsinin POM tarafindan belirlenen erisim sekilleri var.
+
+        ornegin locate'lere obje olusturarak, webDriver'a ise static yolla ulasiyoruz.
+
+        POM dizayn edenler Driver calss'indan obje olusturularak
+        WebDriver kullanimini engellemek icin
+        Singleton Pattern kullanmislar
+
+        Singleton Pattern bir class'dan obje olusturulmasini engelleyen
+        yontemlerden birisi
+
+         */
+
+        Driver driver=new Driver();
+        driver.getDriver().get(ConfigReader.getProperty("amazonUrl"));
+        ReusableMethods.bekle(3);
+        driver.closeDriver();
+    }
+}
