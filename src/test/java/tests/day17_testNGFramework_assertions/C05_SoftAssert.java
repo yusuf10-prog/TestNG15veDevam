ef55ac9702914f92dfeb6028ab1fca6968c4df72
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-07-25 00:33:42 +0300

    25.07.2024

---
+++ src/test/java/tests/day17_testNGFramework_assertions/C05_SoftAssert.java
@@ -0,0 +1,37 @@
+package tests.day17_testNGFramework_assertions;
+
+import org.testng.annotations.Test;
+
+public class C05_SoftAssert {
+    /*
+    TestNG assertion konusunda da bize bir alternatif sunar.
+
+    TestNG iki farklı Assertion class'ina sahiptir
+
+    1-Assert
+        Bu Junit'teki assertion ile bire bir aynidir.
+        sadece isimlendirirken diger alternatif softAssert oldugundan
+        Assert class'ina da HARD ASSERT denir.
+
+        hard assert karsilastigi ilk failed'da
+        calismayi durdurur, dolayisiyla geriye kalan assertion'larin
+        passed veya failed sonuclarindan hangisini alacagini BİLEMEYİZ
+     2-Soft Assert
+        Soft Assert biz raporla diyene kadar
+        yaptigi tum testlerin sonuclarini kendisi tutar test passed de olsa
+        failed de olsa calismaya devam eder.
+
+        ne zaman raporla dersek
+        tum failed olanlari bize rapor eder ve calismayi durdurur.
+
+        eger hic failed olan yoksa
+        class calismaya devam eder.
+
+     */
+
+    @Test
+    public void softAssertionTesti(){
+        //Test icin
+
+    }
+}
