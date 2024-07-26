8c2544885c19bf339bb73e88d0e73b68356ad2dd
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-07-26 23:30:44 +0300

    düzenleme

--- src/test/java/tests/day17_testNGFramework_assertions/Test.java
+++ src/test/java/tests/day17_testNGFramework_assertions/Test.java
@@ -5,8 +5,8 @@ public class Test {
         Animal myDog=new Dog();
         myDog.makeSound(); //Bark
 
-      //  Animal dog=new Animal();
-       // dog.makeSound(); // Some sound
+        Animal dog=new Animal();
+        dog.makeSound(); // Some sound
 
      //   Dog dogi=new Dog();
      //   dogi.makeSound(); // Bark
