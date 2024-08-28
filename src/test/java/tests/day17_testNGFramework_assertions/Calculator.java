c6c92140089ffae244a0309476449f5b92a0fc4d
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-08-29 00:25:53 +0300

    29.08.2024

--- src/test/java/tests/day17_testNGFramework_assertions/Calculator.java
+++ src/test/java/tests/day17_testNGFramework_assertions/Calculator.java
@@ -74,9 +74,8 @@ public class Calculator {
         // 7 tam sayinin toplami
         System.out.println(calc.add(1,2,3,4,5,6,7));
 
-        // 8 tam sayinin toplami
-        System.out.println(calc.add(1,2,3,4,5,6,7, 8));
-
+        // 8 tam sayinin toplam
+        System.out.println(calc.add(1,2,3,4,5,6,7, 8))
 
     }
 }
