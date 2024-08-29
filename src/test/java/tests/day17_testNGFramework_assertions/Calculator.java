b4cbe4c0cb84566976da03569960b38f5585fec2
yusuf10-prog <kuzkayayusuf404@gmail.com>
2024-08-29 14:27:33 +0300

    düzenleme

--- src/test/java/tests/day17_testNGFramework_assertions/Calculator.java
+++ src/test/java/tests/day17_testNGFramework_assertions/Calculator.java
@@ -77,6 +77,8 @@ public class Calculator {
         // 8 tam sayinin toplam
         System.out.println(calc.add(1,2,3,4,5,6,7, 8));
 
+        System.out.println(calc.add(1,23,3,444,55,666666,77777,777566524));
+
     }
 }
 
